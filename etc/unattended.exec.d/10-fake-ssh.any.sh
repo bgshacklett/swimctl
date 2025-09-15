83e5f1775e13de54a6e7ac70b050906a6ab3654e
Brian G. Shacklett <brian@digital-traffic.net>
2025-09-15 17:15:20 -0400

    WIP

--- etc/unattended.exec.d/10-fake-ssh.any.sh
+++ etc/unattended.exec.d/10-fake-ssh.any.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-# trick setup-alpine to pretend existing SSH connection
-# and therefore keep (do not reset) network interfaces while running in background
-# requires alpine-conf 3.15.1 and later, available from Alpine 3.17
-# SSH_CONNECTION="FAKE" setup-alpine -ef /tmp/ANSWERFILE
+. "$BOOT/unattended.lib.sh"
+
+set -x
+LINK_NAME="$(ip -br link)"
+
+_logger "Found link named: $LINK_NAME"
+
+udhcpc -i "$LINK_NAME"
+set +x
